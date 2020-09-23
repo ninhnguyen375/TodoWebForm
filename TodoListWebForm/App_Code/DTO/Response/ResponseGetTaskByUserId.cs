320d07b7aed7dd81e60621ae1365a297e1621fbf
Ram4GB <leminhcuong2988@yahoo.com.vn>
2020-09-23 15:18:40 +0700

    [FIX] Fix Task detail

---
+++ TodoListWebForm/App_Code/DTO/Response/ResponseGetTaskByUserId.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace TodoListWebForm.App_Code.DTO.Response
+{
+    public class ResponseGetTaskByUserId
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public string Status { get; set; }
+        public bool Private { get; set; }
+        public DataTable Partners { get; set; }
+    }
+}
